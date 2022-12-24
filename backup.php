93f43b207c2a319ff04f8e0b2cc89074a862cf95
rifqifazzam <rifqifauzan709@gmail.com>
2022-12-24 15:54:30 +0700

    add

--- backup.php
+++ backup.php
@@ -1,1 +1,17 @@
-
+<!-- Layanan  -->
+<!-- <div class="mb-3 row">
+                        <label for="layanan" class="col-sm-2 col-form-label">layanan</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="layanan" name="layanan" required>
+                                <option value="">Pilih layanan</option>
+                                <option value="Haircut" <?php if ($layanan=="Haircut")
+                                    echo "selected" ?>>Haircut
+                                    </option>
+                                    <option value="Pedicure" <?php if ($layanan=="Pedicure")
+                                        echo "selected" ?>>Pedicure</option>
+                                    <option value="Manicure" <?php if ($layanan=="Manicure")
+                                        echo "selected" ?>>Manicure
+                                    </option>
+                                </select>
+                            </div>
+                        </div> -->
--- reservasi.php
+++ reservasi.php
@@ -112,22 +112,6 @@ if (isset($_POST['submit'])) {
                     </div>
 
                     <!-- lAYANAN -->
-                    <!-- <div class="mb-3 row">
-                        <label for="layanan" class="col-sm-2 col-form-label">layanan</label>
-                        <div class="col-sm-10">
-                            <select class="form-control" id="layanan" name="layanan" required>
-                                <option value="">Pilih layanan</option>
-                                <option value="Haircut" <?php if ($layanan=="Haircut")
-                                    echo "selected" ?>>Haircut
-                                </option>
-                                <option value="Pedicure" <?php if ($layanan=="Pedicure")
-                                    echo "selected" ?>>Pedicure</option>
-                                <option value="Manicure" <?php if ($layanan=="Manicure")
-                                    echo "selected" ?>>Manicure
-                                </option>
-                            </select>
-                        </div>
-                    </div> -->
                     <label for="layanan" class="col-sm-2 col-form-label">layanan</label>
                     <div class="form-check">
                     <input type="checkbox" name="layanan[]" value="Pedicure"> Pedicure<br><?php if ($layanan=="pedicure")
